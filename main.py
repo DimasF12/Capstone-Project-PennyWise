a331f312686325526d56ee1df490babccbae5c13
DimasF12 <dimas.frmn012@gmail.com>
2024-12-13 09:59:31 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from flask import Flask, request
+from flask import Flask, jsonify
+from flask_cors import CORS
+from app.routes.calculator import investment_bp
 
 app = Flask(__name__)
+CORS(app)  # Mengizinkan CORS untuk akses dari frontend
 
-@app.route('/')
-def home():
-    return "Welcome to the Home Page!"
+# Mendaftarkan Blueprint untuk rute investasi
+app.register_blueprint(investment_bp)
 
-@app.route('/calculator', methods=['GET', 'POST'])
-def calculator():
-    if request.method == 'POST':
-        data = request.json
-        return f"POST data received: {data}"
-    return "GET request to Calculator"
+# Rute untuk pengecekan status server
+@app.route('/status', methods=['GET'])
+def check_status():
+    return jsonify({
+        'status': 'success',
+        'message': 'Server is running',
+        'code': 200
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
