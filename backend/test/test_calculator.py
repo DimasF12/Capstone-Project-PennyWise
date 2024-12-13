b0d091d6ef0e3310fb1352f08597b9e91a024056
DimasF12 <dimas.frmn012@gmail.com>
2024-12-13 08:55:45 +0700

    Delete backend directory

--- backend/test/test_calculator.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from app.service.calculator import calculate_future_value
-
-def test_calculate_future_value():
-    result = calculate_future_value(1000, 500, 10, 5)
-    assert result > 0
