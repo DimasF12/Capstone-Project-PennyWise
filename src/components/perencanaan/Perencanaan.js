0f6e37a7f9c7d5ef54ef0cabdb5a4237a52d8ac5
Rakhaa-rgb <rakhamadina260@gmail.com>
2024-12-16 13:56:56 +0700

    Update Perencanaan.js

--- src/components/perencanaan/Perencanaan.js
+++ src/components/perencanaan/Perencanaan.js
@@ -9,6 +9,22 @@ import { faCar } from '@fortawesome/free-solid-svg-icons';
 const Perencanaan = ({ username }) => {
   const navigate = useNavigate(); // Hook untuk navigasi
 
+  const handleFeatureClick = (feature) => {
+    switch (feature) {
+      case 'barangImpian':
+        navigate('/BarangImpian'); // Replace with your actual route
+        break;
+      case 'danaDarurat':
+        navigate('/DanaDarurat'); // Replace with your actual route
+        break;
+      case 'danaPensiun':
+        navigate('/dana-pensiun'); // Replace with your actual route
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="perencanaan-container">
       {/* Back Button */}
@@ -28,23 +44,23 @@ const Perencanaan = ({ username }) => {
       <div className="perencanaan-section-title">Ayo Prediksi Target mu 😎</div>
 
       {/* Feature Cards */}
-      <div className="perencanaan-feature-group">
-            <div className="perencanaan-feature-box">
-                <div className="perencanaan-feature-icon">
-                   <FontAwesomeIcon icon={faCar} size='2xl'></FontAwesomeIcon>
-                </div>
-                <div className="perencanaan-feature-text">Barang impian</div>
-            </div>
-            <div className="perencanaan-feature-box">
-                <div className="perencanaan-feature-icon">
-                  <FontAwesomeIcon icon={faWallet} size="2xl" />
-                </div>
-                <div className="perencanaan-feature-text">Dana darurat</div>
-            </div>
-            <div className="perencanaan-feature-box">
-                <div className="perencanaan-feature-icon">
-                  <FontAwesomeIcon icon={faPersonCane} size='2xl'></FontAwesomeIcon>
-                </div>
+       <div className="perencanaan-feature-group">
+        <div className="perencanaan-feature-box" onClick={() => handleFeatureClick('barangImpian')}>
+          <div className="perencanaan-feature-icon">
+            <FontAwesomeIcon icon={faCar} size='2xl' />
+          </div>
+          <div className="perencanaan-feature-text">Barang impian</div>
+        </div>
+        <div className="perencanaan-feature-box" onClick={() => handleFeatureClick('danaDarurat')}>
+          <div className="perencanaan-feature-icon">
+            <FontAwesomeIcon icon={faWallet} size="2xl" />
+          </div>
+          <div className="perencanaan-feature-text">Dana darurat</div>
+        </div>
+        <div className="perencanaan-feature-box" onClick={() => handleFeatureClick('danaPensiun')}>
+          <div className="perencanaan-feature-icon">
+            <FontAwesomeIcon icon={faPersonCane} size='2xl' />
+          </div>
                 <div className="perencanaan-feature-text">Dana pensiun</div>
             </div>
         </div>
