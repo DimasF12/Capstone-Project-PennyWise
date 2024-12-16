0c346451d444c8a7bc67ea6e368574e6751a7594
Rakhaa-rgb <rakhamadina260@gmail.com>
2024-12-16 13:58:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import KalkulatorInvestasi from './components/kalkulator/KalkulatorInvestasi';
 import Perencanaan from './components/perencanaan/Perencanaan';
 import TrackingKeuangan from './components/tracking/TrackingKeuangan';
 import HasilStrategi from './components/hasil/HasilStrategi';
+import BarangImpian from './components/perencanaan/barangimpian/BarangImpian' ;
+import HasilBarangImpian from './components/perencanaan/barangimpian/hasilbarang/HasilBarangImpian';
+import DanaDarurat from './components/perencanaan/danadarurat/DanaDarurat' ;
+import HitungDanaDarurat from './components/perencanaan/danadarurat/HitungDana/HitungDanaDarurat';
 
 const App = () => {
   return (
@@ -25,6 +29,19 @@ const App = () => {
 
           {/* Route ke halaman hasil strategi */}
           <Route path="/hasil/HasilStrategi" element={<HasilStrategi username="User" />} />
+
+          {/* Route ke halaman barang impian */}
+           <Route path="/BarangImpian" element={<BarangImpian />} username="User" /> 
+
+           {/* Route ke halaman hasil barang impian */}
+           <Route path="/hasilBarang/HasilBarangImpian" element={<HasilBarangImpian />} username="User" /> 
+
+           {/* Route ke halaman dana darurat */}
+           <Route path="/DanaDarurat" element={<DanaDarurat />} username="User" />
+
+           {/* Route ke halaman hitung barang impian */}
+           <Route path="/HitungDana/HitungDanaDarurat" element={<HitungDanaDarurat/>} username="User" /> 
+             
         </Routes>
       </div>
     </Router>
